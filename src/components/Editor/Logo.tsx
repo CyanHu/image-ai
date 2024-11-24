fc079c74bfa8b6f99a3681449ad7a2b3c9ba6b55
CyanHu <1205430144@qq.com>
2024-11-24 20:30:40 +0800

    feat: internationalized logo

--- src/components/Editor/Logo.tsx
+++ src/components/Editor/Logo.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useTranslations } from 'next-intl';
 
 const Logo = () => {
+  const t = useTranslations('Logo');
+  
   return (
     <Link href="/">
       <div className="size-8 relative shrink-0">
@@ -9,7 +12,7 @@ const Logo = () => {
           className="shrink-0 hover:opacity-75 transition"
           src="/logo.svg"
           fill
-          alt="Image AI"
+          alt={t('alt')}
         />
       </div>
     </Link>
